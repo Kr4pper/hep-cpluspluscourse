ad767ae6536ace25e8da1b41110a683a09a30c39
Kr4pper <Kr4pper@github.com>
2025-02-12 17:37:52 +0100

    solved basicTypes exercise

--- exercises/basicTypes/basicTypes.cpp
+++ exercises/basicTypes/basicTypes.cpp
@@ -12,68 +12,69 @@
  * - Answer the questions in the code
  */
 
-int main() {
-  std::cout << "Using literals of different number types:\n";
-  print(5);
-  print(5/2);           //FIXME
-  print(100/2ull);
-  print(2 + 4ull);
-  print(2.f + 4ull);
-  print(0u - 1u);       // FIXME
-  print(1.0000000001f); // FIXME Why is this number not represented correctly?
-  print(1. + 1.E-18);   // FIXME
+int main()
+{
+    std::cout << "Using literals of different number types:\n";
+    print(5);
+    print(5 / 2.f); // specify as float
+    print(100 / 2ull);
+    print(2 + 4ull);
+    print(2.f + 4ull);
+    print(0 - 1); // result of unsigned operation cant be negative
+    print(1.0000000001f); // FIXME Why is this number not represented correctly? -> float too small, need double?
+    print(1.l + 1.E-18); // FIXME should be working with long double but doesnt
 
-  std::cout << "\nUsing increment and decrement operators:\n";
-  int a = 1;
-  int b;
-  int c;
-  print(b = a++);       // Q: What is the difference between a++ and ++a?
-  print(c = ++a);
-  print(a);
-  print(b);
-  print(c);
+    std::cout << "\nUsing increment and decrement operators:\n";
+    int a = 1;
+    int b;
+    int c;
+    print(b = a++);
+    print(c = ++a);
+    print(a);
+    print(b);
+    print(c);
 
-  std::cout << "\nCompound assignment operators:\n";
-  int n = 1;
-  print(n *= 2);        // Q: Is there a difference between this and the next line?
-  print(n *= 2.9);
-  print(n -= 1.1f);
-  print(n /= 4);        // Q: Based on the results of these expressions, is there a better type to be used for n?
+    std::cout << "\nCompound assignment operators:\n";
+    int n = 1;
+    print(n *= 2); // Q: Is there a difference between this and the next line? -> 2 * 2.9 gets truncated to 5
+    print(n *= 2.9);
+    print(n -= 1.1f);
+    print(n /= 4); // Q: Based on the results of these expressions, is there a better type to be used for n? -> float
 
-  std::cout << "\nLogic expressions:\n";
-  const bool alwaysTrue = true;
-  bool condition1 = false;
-  bool condition2 = true;
-  print( alwaysTrue && condition1 && condition2 );
-  print( alwaysTrue || condition1 && condition2 );  // Q: Why does operator precedence render this expression useless?
-  print( alwaysTrue && condition1 || condition2 );
-  print(condition1 != condition1);                  // Q: What is the difference between this and the following expression?
-  print(condition2 = !condition2);
-  print( alwaysTrue && condition1 && condition2 );
-  print( alwaysTrue || condition1 && condition2 );
-  print( alwaysTrue && condition1 || condition2 );
+    std::cout << "\nLogic expressions:\n";
+    const bool alwaysTrue = true;
+    bool condition1 = false;
+    bool condition2 = true;
+    print(alwaysTrue && condition1 && condition2);
+    print(alwaysTrue || condition1 && condition2); // Q: Why does operator precedence render this expression useless? -> c1 && c2 not evaluated since already truthy before
+    print(alwaysTrue && condition1 || condition2);
+    print(condition1 != condition1); // Q: What is the difference between this and the following expression? -> comparison vs assignment of inverse
+    print(condition2 = !condition2);
+    print(alwaysTrue && condition1 && condition2);
+    print(alwaysTrue || condition1 && condition2);
+    print(alwaysTrue && condition1 || condition2);
 
-  std::cout << '\n';
-  print( false || 0b10 );     // Q: What is the difference between || and | ?
-  print( false | 0b10 );
-  printBinary( 0b1 & 0b10 );
-  printBinary( 0b1 | 0b10 );
-  printBinary( 0b1 && 0b10 ); // Q: Are the operators && and || appropriate for integer types?
-  printBinary( 0b1 || 0b10 );
+    std::cout << '\n';
+    print(false || 0b10); // Q: What is the difference between || and | ? -> logical or vs binary or
+    print(false | 0b10);
+    printBinary(0b1 & 0b10);
+    printBinary(0b1 | 0b10);
+    printBinary(0b1 && 0b10); // Q: Are the operators && and || appropriate for integer types? -> no since most integers are truthy
+    printBinary(0b1 || 0b10);
 
-  std::cout << "\nPlay with characters and strings:\n";
-  print("a");                 // Q: Why is this expression two bytes at run time, the next only one?
-  print('a');
+    std::cout << "\nPlay with characters and strings:\n";
+    print("a"); // Q: Why is this expression two bytes at run time, the next only one? -> NUL terminator
+    print('a');
 
-  char charArray[20];
-  char* charPtr = charArray;
-  charArray[19] = 0; // Make sure that our string is terminated with the null byte
+    char charArray[20] = "__cdefghijklmnopqrs";
+    char* charPtr = charArray;
+    charArray[19] = '\0'; // Make sure that our string is terminated with the null byte
 
-  print(charArray);
-  print(charArray[0] = 'a');
-  print(charArray);
-  print(charArray[1] = 98);
-  print(charArray);
-  print(charPtr);
-  // FIXME: Ensure that no unexpected garbage is printed above
+    print(charArray);
+    print(charArray[0] = 'a');
+    print(charArray);
+    print(charArray[1] = 98);
+    print(charArray);
+    print(charPtr);
+    // FIXME: Ensure that no unexpected garbage is printed above
 }
