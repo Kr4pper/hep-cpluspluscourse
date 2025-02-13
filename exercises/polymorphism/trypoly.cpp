50be4fefa0b0a7996dfeaa2d0fc37d19f60ea244
Kr4pper <Kr4pper@github.com>
2025-02-13 18:21:35 +0100

    solved polymorphism exercise

--- exercises/polymorphism/trypoly.cpp
+++ exercises/polymorphism/trypoly.cpp
@@ -1,16 +1,19 @@
 #include "Polygons.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
     // create a Pentagon, call its perimeter method
-
+    Pentagon pentagon = Pentagon(1);
+    pentagon.computePerimeter();
 
     // create a Hexagon, call its perimeter method
-
+    Hexagon hexagon = Hexagon(1);
+    hexagon.computePerimeter();
 
     // create a Hexagon, call the perimeter method through a reference to Polygon
-
+    RegularPolygon hexagon2 = Hexagon(1);
+    hexagon2.computePerimeter();
 
     // retry virtual method
-
 }
