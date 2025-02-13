d89fa3ab3d249216609a93218b18aaa0e8b51217
Kr4pper <Kr4pper@github.com>
2025-02-13 18:07:43 +0100

    solved loopsRefsAuto exercise

--- exercises/loopsRefsAuto/loopsRefsAuto.cpp
+++ exercises/loopsRefsAuto/loopsRefsAuto.cpp
@@ -1,40 +1,48 @@
 #include <iostream>
 
 struct DontCopyMe {
-   int resultA;
-   int resultB;
+    int resultA;
+    int resultB;
 
-   // This is material for the second day:
-   DontCopyMe() = default;
-   DontCopyMe(const DontCopyMe& other) :
-       resultA(other.resultA),
-       resultB(other.resultB)
-   { std::cout << "Please don't copy me\n"; }
+    // This is material for the second day:
+    DontCopyMe() = default;
+    DontCopyMe(const DontCopyMe& other)
+        : resultA(other.resultA)
+        , resultB(other.resultB)
+    {
+        std::cout << "Please don't copy me\n";
+    }
 };
 
-int main() {
-   // We create an array of DontCopyMe structs:
-   DontCopyMe collection[10];
+int main()
+{
+    // We create an array of DontCopyMe structs:
+    DontCopyMe collection[10];
 
-   // Task 1:
-   // Write a for loop that initialises each struct's resultA and resultB with ascending integers.
-   // Verify the output of the program before and after you do this.
+    // Task 1:
+    // Write a for loop that initialises each struct's resultA and resultB with ascending integers.
+    // Verify the output of the program before and after you do this.
+    int i = 0;
+    for (auto& item : collection) {
+        item.resultA = i;
+        item.resultB = i + 1;
+        i++;
+    }
 
+    // Task 2:
+    // We use a range-based for loop to analyse the array of structs.
+    // The problem is: we are copying every DontCopyMe ...
+    // Fix this loop using references.
+    // Hint: Fix the type declaration "auto" in the loop head.
+    int resultA = 0;
+    int resultB = 0;
+    for (auto const& item : collection) {
+        resultA += item.resultA;
+        resultB += item.resultB;
+    }
+    std::cout << "resultA = " << resultA << "\tresultB = " << resultB << "\n";
 
-   // Task 2:
-   // We use a range-based for loop to analyse the array of structs.
-   // The problem is: we are copying every DontCopyMe ...
-   // Fix this loop using references.
-   // Hint: Fix the type declaration "auto" in the loop head.
-   int resultA = 0;
-   int resultB = 0;
-   for (auto item : collection) {
-      resultA += item.resultA;
-      resultB += item.resultB;
-   }
-   std::cout << "resultA = " << resultA << "\tresultB = " << resultB << "\n";
-
-   return 0;
+    return 0;
 }
 
 // Task 3:
